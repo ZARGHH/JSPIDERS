abd065b7dd8f59c7cf4b7ec2df3ed2aa88d58189
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-07 22:26:22 +0530

    Add files via upload

---
+++ INT.java
@@ -0,0 +1,26 @@
+package simple.java;
+import java.util.Scanner;
+
+public class INT {
+
+	public static void main(String[] args) 
+	{
+Scanner sc=new Scanner(System.in);
+System.out.println(" the integeral values are");
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+
+ZARGHAM(a,b);//method calling
+
+	}
+
+static void ZARGHAM( int a ,int b)
+{
+	System.out.println("the integral values are"+a);
+	System.out.println("the integral value  are"+b);
+	
+
+	}
+
+}
