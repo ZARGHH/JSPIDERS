722920901b3da24fc6804892eb87c34f15d8eea4
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-08 15:42:48 +0530

    Add files via upload

---
+++ FORM.java
@@ -0,0 +1,20 @@
+package simple.java;
+import java .util.Scanner;
+public class FORM {
+
+	public static void main(String[] args) 
+	{
+
+Scanner sc=new Scanner(System.in);
+	
+	System.out.println(" ENTER YOUR NAME");
+int p=sc.nextInt();	 
+	 if (p==3)
+		 System.out.println("Zargham");
+	 else if(p>3&&p<=5)
+		 System.out.println("ZARGHAM AHMED");
+	 else
+		 System.out.println("zaimy");
+	}
+
+}
