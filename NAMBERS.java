065c3145a245d0036416655695fc6d804e8a2b10
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-09 16:58:27 +0530

    Add files via upload

---
+++ NAMBERS.java
@@ -0,0 +1,15 @@
+package simple.java;
+import java.util.Scanner;
+
+public class NAMBERS {
+
+	public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your number");
+int n=sc.nextInt();
+for(int i=0;i<=n;i++)
+	System.out.println(i);
+	}
+
+}
