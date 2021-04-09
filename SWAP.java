f7d7bd282a83f3285ae1891ff6fcef87e91c8f3e
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-09 11:58:31 +0530

    Add files via upload

---
+++ SWAP.java
@@ -0,0 +1,27 @@
+package simple.java;
+
+public class SWAP 
+{
+	public static void main(String args[])
+	{
+	int a= 75;
+	int b=65;
+	 int c=a;
+	a=b;
+	 b=c;
+	 System.out.println("after swapping a"+a+"after swapping b"+b);
+}
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
