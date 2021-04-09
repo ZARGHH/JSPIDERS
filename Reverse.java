7fb4611947668bf1d1139331529746aa78f8aa67
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-09 17:06:47 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+package simple.java;
+import java.util.Scanner;
+
+public class Reverse {
+
+	public static void main(String[] args) 
+	{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your number");
+int n=sc.nextInt();
+for(int i=n;i>=1;i--)
+System.out.println(i);
+	}
+
+}
