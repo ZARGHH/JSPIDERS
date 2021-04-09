57f531be5b7267d2da620d7d1cdbf2ae1a4836e4
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-09 17:50:20 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -6,17 +6,22 @@ public class Prime {
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("enter a number");
-	int a=sc.nextInt();
-	int b=sc.nextInt();
-	int c=a/b;
-if
-(c!=0)
-{
-	System.out.println("the number is  not prime");
-}
-	else
+	int n=sc.nextInt();
+	int i;
+	if(n==1) 
 	{
-		System.out.println(" the number is is prime");
+		System.out.println("prime number starts with 2");
+		
 	}
-}
+	for( i=2;i<n;i++)
+	{
+		if(n%i==0)
+			System.out.println("number is  not prime");
+		break;
+	}
+	if(n==i)
+
+		System.out.println("it is  prime");
+	
+	}
 }
