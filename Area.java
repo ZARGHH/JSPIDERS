2373b577348ccbe6860aea8cf85aef9702a80a70
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-11 20:04:55 +0530

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,5 +1,40 @@
 package simple.java;
+import java.util.Scanner;
 
-public class Area {
+public class Area 
+{
+	
+
+	public static void main(String[] args)
+	{
+
+		   Scanner sc = new Scanner(System.in);
+	        System.out.println("enter the number");
+	        int s = sc.nextInt();
+	        int l = sc.nextInt();
+	        int b = sc.nextInt();
+	        int h = sc.nextInt();
+	        
+	       area(s);
+	       area(l,b);
+area(l,b,h);
+
+	    }
+
+	    static void area(int a)
+	    {
+	       System.out.println(a*a) ;
+	    }
+
+	    static void area(int l, int b)
+	    {
+	        System.out.println(l * b);
+	    }
+
+	    static void area(int l, int b, int h)
+	    {
+	        System.out.println(l * b * h);
+	}
+
 
 }
