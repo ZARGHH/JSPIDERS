7e50ca22744294a7085569bc6f2c76e2f6b624bb
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-04-22 11:42:28 +0530

    Add files via upload

--- Days.java
+++ Days.java
@@ -3,48 +3,19 @@ import java.util.Scanner;
 
 public class Days {
 
-	public static void main(String[] args) 
-	{
-Scanner sc=new Scanner(System.in);
-System.out.println("enter the day number");	
-int s=sc.nextInt();
-
-if(s==1)
-{
-	System.out.println("SUNDAY");
+	public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+ int s=sc.nextInt();
+ int m,y,c,r;
+ m=s*30;
+ y=s*365;
+ c=s/30;
+ r=s/365;
+ 
+ System.out.println("the conversion of  month to days is"+m);
+ System.out.println("the conversions yeras to days is "+y);
+ System.out.println("the conversion  of days to month is"+c);
+ System.out.println("the conversion of days to year is"+r);
 	}
-if(s==2)
-{ System.out.println("MONDAY");
-}
-if(s==3)
-{
-	System.out.println("TUESDAY");
-}
-if (s==4)
-{
-	System .out.println("WEDNESDAY");
-}
-if(s==5)
-{
-	System.out.println("THURSDAY");
-}
-if(s==6)
-{
-	System.out.println("FRIDAY");
-}
-if(s==7)
-{
-	System.out.println("SATURDAY");
-}
-if
-(s>7)
-{
-	System.out.println("INVALID CHOICE");
-}
 
-
-
-	}
 }
-
-
